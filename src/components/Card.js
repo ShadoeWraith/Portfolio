89bb9391221d3f8c44b60ca4e69aff7c9f7e279d
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-07-30 23:45:44 -0700

    added project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,76 +1,64 @@
 import AirPollution from '../assets/AirPollution.jpg';
 import ParkPlace from '../assets/ParkPlace.jpg';
 import CounselorReservation from '../assets/CounselorReservation.jpg';
+import tranquility from '../assets/tranquility.jpg';
 
 import github from '../assets/github.jpg';
 import deploy from '../assets/deploy.jpg';
 
 export default function Card() {
+  const projectData = [
+    {
+      title: 'Air Quality',
+      image: AirPollution,
+      github: 'https://callank21.github.io/group-project/index.html',
+      deployed: 'https://github.com/Callank21/group-project',
+    },
+    {
+      title: 'Park Place',
+      image: ParkPlace,
+      github:
+        'https://github.com/SalvadorBanuelos424/Park-Place-master-planned-community',
+      deployed: 'https://park-place-association.herokuapp.com/',
+    },
+    {
+      title: 'Counselor Reservation',
+      image: CounselorReservation,
+      github: 'https://github.com/uhMammoth/school-website',
+      deployed: 'https://mern-school-website.herokuapp.com/',
+    },
+    {
+      title: 'FFXIV Tranquility',
+      image: tranquility,
+      github: 'https://github.com/ShadoeWraith/tranquility',
+      deployed: 'https://shadoewraith.github.io/tranquility/',
+    },
+  ];
+
   return (
     <div className="card-container">
-      <div className="card">
-        <div className="card-title">
-          <h4>Air Pollution</h4>
-        </div>
-        <div className="image-container">
-          <img
-            className="card-image"
-            src={AirPollution}
-            alt="Air Pollution Project"
-          />
-        </div>
-        <div className="card-footer">
-          <a href="https://github.com/Callank21/group-project">
-            <img className="card-icon" src={github} alt="github" />
-          </a>
-          <a href="https://callank21.github.io/group-project/index.html">
-            <img className="card-icon" src={deploy} alt="github" />
-          </a>
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-title">
-          <h4>Park Place</h4>
-        </div>
-        <div className="image-container">
-          <img
-            className="card-image"
-            src={ParkPlace}
-            alt="Park Place Project"
-          />
-        </div>
-        <div className="card-footer">
-          <a href="https://github.com/SalvadorBanuelos424/Park-Place-master-planned-community">
-            <img className="card-icon" src={github} alt="github" />
-          </a>
-          <a href="https://park-place-association.herokuapp.com/">
-            <img className="card-icon" src={deploy} alt="github" />
-          </a>
-        </div>
-      </div>
-
-      <div>
+      {projectData.map((project) => (
         <div className="card">
           <div className="card-title">
-            <h4>Counselor Reservation</h4>
+            <h4>{project.title}</h4>
           </div>
           <div className="image-container">
             <img
               className="card-image"
-              src={CounselorReservation}
-              alt="Counselor Reservation Site"
+              src={project.image}
+              alt="Air Pollution Project"
             />
           </div>
           <div className="card-footer">
-            <a href="https://github.com/Callank21/group-project">
+            <a href={project.github}>
               <img className="card-icon" src={github} alt="github" />
             </a>
-            <a href="https://mern-school-website.herokuapp.com/">
+            <a href={project.deployed}>
               <img className="card-icon" src={deploy} alt="github" />
             </a>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
